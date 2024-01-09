7278518ae55bdb40cac36e6fd1d69214ae99546d
jibrilosman <jibrilosman404@gmail.com>
2024-01-09 17:48:51 -0500

    modified file: src/components/details/Detail.jsx

--- src/components/details/Detail.jsx
+++ src/components/details/Detail.jsx
@@ -1,11 +1,14 @@
 import { useData } from "../../context/DataContext";
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 import "./Detail.css";
 import { FaArrowLeft } from "react-icons/fa";
+import { useEffect, useState } from "react";
 
 const Detail = () => {
   const { data } = useData();
   const { id } = useParams();
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
 
   const formatPopulation = (population) => {
     return population.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
@@ -14,6 +17,20 @@ const Detail = () => {
   const handleArrowClick = () => {
     window.history.back();
   };
+
+  const handleBorderClick = (border) => {
+    const borderCountry = data.find((country) => country.cca3 === border);
+    const borderCommonName = borderCountry?.name.common || border;
+    navigate(`/detail/${borderCommonName}`);
+  };
+
+
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 1000);
+  }, []);
  
 
   return (
@@ -24,7 +41,96 @@ const Detail = () => {
           <span>back</span>
         </div>
 
-        {data &&
+        {loading ? (
+          <div className="loading">Loading...</div>
+        ) : (
+          data &&
+          data
+            .filter((item) => item.name.common === id)
+            .map((item, index) => (
+              <article key={index} className="ct">
+                <div className="image">
+                  <img src={item.flags.png} alt={item.name.common} />
+                </div>
+                <div className="info-detail">
+                  <div className="column">
+                    <h2>{item.name.common}</h2>
+                    <p>
+                      <strong>Native Name:</strong> {item.name.common}
+                    </p>
+                    <p>
+                      <strong>Population:</strong>{" "}
+                      {formatPopulation(item.population)}
+                    </p>
+                    <p>
+                      <strong>Region:</strong>
+                      {item.region}
+                    </p>
+                    <p>
+                      <strong>Sub Region: </strong>
+                      {item.subregion}
+                    </p>
+                    <p>
+                      <strong>Capital:</strong> {item.capital}
+                    </p>
+                  </div>
+                  <div className="column">
+                    <p>
+                      <strong>Top Level Domain:</strong> {item.tld}
+                    </p>
+                    <p>
+                      Languages:{" "}
+                      {Object.values(item.languages).map((lang, langIndex) => (
+                        <span key={langIndex}>
+                          {lang}
+                          {langIndex < Object.values(item.languages).length - 1
+                            ? ", "
+                            : ""}
+                        </span>
+                      ))}
+                    </p>
+                    <p>
+                      Currencies:{" "}
+                      {Object.values(item.currencies).map((curr, currIndex) => (
+                        <span key={currIndex}>
+                          {` " ${curr.symbol} "  ${curr.name}`}
+                          {currIndex < Object.values(item.currencies).length - 1
+                            ? ", "
+                            : ""}
+                        </span>
+                      ))}
+                    </p>
+
+                    <p>
+                      Borders:{" "}
+                      {item.borders
+                        ? Object.values(item.borders).map(
+                            (border, borderIndex) => (
+                              <span key={borderIndex}>
+                                <button 
+                                 style={{padding:"4px", borderRadius:"2px"}}
+                                  onClick={() => handleBorderClick(border)}
+                                >
+                                 {data.find((country) => country.cca3 === border)?.name.common || border}
+                                </button>
+                                {borderIndex <
+                                Object.values(item.borders).length - 1
+                                  ? ", "
+                                  : ""}
+                              </span>
+                            ) 
+                          )
+                        : ""}
+                    </p>
+                  </div>
+                </div>
+              </article>
+            ))
+        )}
+
+
+
+        {/* {data &&
           data
             .filter((item) => item.name.common === id)
             .map((item, index) => (
@@ -101,7 +207,7 @@ const Detail = () => {
                   </div>
                 </div>
               </article>
-            ))}
+            ))}  */}
       </div>
     </>
   );
